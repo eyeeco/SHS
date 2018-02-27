cacc2782071ced298c27b39d2aaab81f7eeb7be1
wangyang <1466738696@qq.com>
2018-02-27 10:48:16 +0800

    作业列表和删除

--- HOMEWORK/urls.py
+++ HOMEWORK/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     # url(r'^$', views.ProjectIndex.as_view(), name='index'),
     url(r'^$', views.HomeworkAdd.as_view(), name='add'),
-    url(r'^list/$', views.HomeworkList.as_view(), name='list'),
+    url(r'^list/$', views.Uploadlist.as_view(), name='list'),
     url(r'^upload/$', views.UploadAdd.as_view(), name='upload'),
+    url(r'^cancel/(?P<uid>.+)$', views.UploadCancel.as_view(),
+        name='cancel'),
 ]

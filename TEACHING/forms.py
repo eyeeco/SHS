918e11e67c4d02bf703835e376ca98d916b7d15d
wangyang <1466738696@qq.com>
2018-03-03 16:24:02 +0800

    注释作业模板格式要求

--- TEACHING/forms.py
+++ TEACHING/forms.py
@@ -5,13 +5,14 @@ from TEACHING.models import UploadTeacher
 
 
 class FileUploadForm(ModelForm):
+    """
     def clean(self):
         cleaned_data = super(FileUploadForm, self).clean()
         extension = self.cleaned_data.get('file_field').name.split('.')[-1]
         if extension not in ["docx"]:
             raise forms.ValidationError('Filetype Error!')
         return cleaned_data
-
+    """
     class Meta:
         model = UploadTeacher
         fields = ['file_field']
